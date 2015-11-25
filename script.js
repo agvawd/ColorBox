29bbf83c51386dc11643587e660e7fdaf8f7799e
Allen <agvawd@hotmail.com>
2015-11-25 11:19:13 -0700

    Changed up the function to work with hex colors passed in to change the color

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 $(document).ready(function(){
-	$("#colorList").change(function(){
-    	if($("option:selected").text() === "Select one..."){
-    		$(".color").slideUp();
-    	}
-    	else{
-        	$(".color").removeClass().addClass("color");
-	        $(".color").addClass($(this).val());
-	        $(".color").slideDown();
-    	}
-    });               
+    // $("#colorList").change(function(test){
+    // 	if($("option:selected").text() === "Select one..."){
+    // 		$(".color").slideUp();
+    // 	}
+    // 	else{
+    //     	$(".color").removeClass().addClass("color");
+	   //      $(".color").addClass($(this).val());
+	   //      $(".color").slideDown();
+    // 	}
+    // });   
+
+    $("#colorList").change(function(test){
+        if($("option:selected").text() === "Select one..."){
+            $(".color").slideUp();
+        }
+        else{
+            // $(".color").removeClass().addClass("color");
+            console.log($(this).val());
+            $(".color").css('background', $(this).val());
+            $(".color").slideDown();
+        }
+     })            
 });
