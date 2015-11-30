740bccf150b8d58a7557b459b14aff2c0ed3661f
Allen Vawdrey <agvawd@hotmail.com>
2015-11-30 14:41:09 -0700

    Changed the function to get the hex value from a different attribute like title

--- script.js
+++ script.js
@@ -5,20 +5,35 @@ $(document).ready(function(){
     // 	}
     // 	else{
     //     	$(".color").removeClass().addClass("color");
-	   //      $(".color").addClass($(this).val());
-	   //      $(".color").slideDown();
+    //      $(".color").addClass($(this).val());
+    //      $(".color").slideDown();
     // 	}
     // });   
+
+    // $("#colorList").change(function(){
+    //     if($("option:selected").text() === "Select one..."){
+    //         $(".color").slideUp();
+    //     }
+    //     else{
+    //         // $(".color").removeClass().addClass("color");
+    //         console.log($(this).val());
+    //         $(".color").css('background', $(this).val());
+    //         $(".color").slideDown();
+    //     }
+    // })   
+
+    //------- Line is used in the real site --------
+    // $("#colorList").live("change", function(){
 
     $("#colorList").change(function(){
         if($("option:selected").text() === "Select one..."){
             $(".color").slideUp();
         }
         else{
-            // $(".color").removeClass().addClass("color");
-            console.log($(this).val());
-            $(".color").css('background', $(this).val());
+            var hexColor = $(this).find("option:selected").attr("title");
+            console.log(hexColor);
+            $(".color").css('background', hexColor);
             $(".color").slideDown();
         }
-     })            
+    })             
 });
